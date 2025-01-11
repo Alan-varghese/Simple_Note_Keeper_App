4cd4ff8e43d07eadf7948b5c59baa41957a85236
Alan Varghese <alanvz757@gmail.com>
2025-01-11 21:21:53 +0530

    Intial Commit

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NoteKeeperApp());
+}
+
+class NoteKeeperApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Note Keeper',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: NotesScreen(),
+    );
+  }
+}
+
+class NotesScreen extends StatefulWidget {
+  @override
+  _NotesScreenState createState() => _NotesScreenState();
+}
+
+class _NotesScreenState extends State<NotesScreen> {
+  List<String> notes = [];
+
+  void addNote(String note) {
+    setState(() {
+      notes.add(note);
+    });
+  }
+
+  void deleteNote(int index) {
+    setState(() {
+      notes.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Note Keeper')),
+      body: ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(notes[index]),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () => deleteNote(index),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddNoteScreen()),
+          );
+          if (result != null) {
+            addNote(result);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class AddNoteScreen extends StatelessWidget {
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Note')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(hintText: 'Enter your note here'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, controller.text);
+              },
+              child: Text('Save Note'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
